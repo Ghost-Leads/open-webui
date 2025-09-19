31dc7224a1c754a3b9aa7e48e9b303a7394cca12
aditya3062 <aditya@audiencexq.com>
2025-09-19 14:25:47 +0545

    Update favicon assets and references in Sidebar component
    - Replace outdated favicon paths in Sidebar.svelte to point to the correct asset locations.
    - Update favicon.ico and favicon-96x96.png with new branding assets.
    - Modify favicon.svg to include updated SVG content for improved visual consistency.

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -536,7 +536,7 @@
 						<div class=" self-center flex items-center justify-center size-9">
 							<img
 								crossorigin="anonymous"
-								src="{WEBUI_BASE_URL}/static/static/favicon-dark.png"
+								src="{WEBUI_BASE_URL}/static/favicon-dark.png"
 								class="sidebar-new-chat-icon size-6 rounded-full group-hover:hidden"
 								alt=""
 							/>
@@ -714,7 +714,7 @@
 				>
 					<img
 						crossorigin="anonymous"
-						src="{WEBUI_BASE_URL}/static/static/favicon-dark.png"
+						src="{WEBUI_BASE_URL}/static/favicon-dark.png"
 						class="sidebar-new-chat-icon size-6 rounded-full"
 						alt=""
 					/>
